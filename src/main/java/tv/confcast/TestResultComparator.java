c2559c908ca6df86b7d8c76eb31a42b5d76d82da
none <none@nowhere.com>
2021-01-06 21:54:39 +0400

    Task 3318: fixes.

--- src/main/java/tv/confcast/TestResultComparator.java
+++ src/main/java/tv/confcast/TestResultComparator.java
@@ -18,11 +18,9 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.Callable;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -44,10 +42,10 @@ public class TestResultComparator implements Callable<Integer> {
     private String comparisonResultsXls;
 
     @NotNull
-    private static Map<String, UrlStat> loadResults(
+    private static LinkedHashMap<String, UrlStat> loadResults(
             @NotNull Path taskFile
     ) throws Exception {
-        HashMap<String, List<Integer>> latencyMap = new HashMap<>();
+        LinkedHashMap<String, List<Integer>> latencyMap = new LinkedHashMap<>();
         try (Reader in = new FileReader(taskFile.toFile())) {
             Iterable<CSVRecord> records = CSVFormat.DEFAULT
                     .withFirstRecordAsHeader()
@@ -66,11 +64,11 @@ public class TestResultComparator implements Callable<Integer> {
                         .add(latency);
             }
         }
-        Map<String, UrlStat> result = new HashMap<>();
+        LinkedHashMap<String, UrlStat> result = new LinkedHashMap<>();
         for (Map.Entry<String, List<Integer>> entry : latencyMap.entrySet()) {
-            IntStream intStream = entry.getValue().stream()
+            Supplier<IntStream> streamProvider = () -> entry.getValue().stream()
                     .mapToInt(Integer::intValue);
-            int average = (int) intStream
+            int average = (int) streamProvider.get()
                     .average()
                     .orElseThrow();
             int median = (int) new Median().evaluate(
@@ -78,8 +76,8 @@ public class TestResultComparator implements Callable<Integer> {
                             .mapToDouble(Integer::doubleValue)
                             .toArray()
             );
-            int min = intStream.min().orElseThrow();
-            int max = intStream.max().orElseThrow();
+            int min = streamProvider.get().min().orElseThrow();
+            int max = streamProvider.get().max().orElseThrow();
             result.put(entry.getKey(), new UrlStat(
                     min,
                     max,
@@ -91,14 +89,14 @@ public class TestResultComparator implements Callable<Integer> {
     }
 
     @NotNull
-    private static Map<String, UrlComparison> compareResults(
+    private static LinkedHashMap<String, UrlComparison> compareResults(
             @NotNull Map<String, UrlStat> referenceResults,
             @NotNull Map<String, UrlStat> resultsToCompare
     ) throws Exception {
         if (!referenceResults.keySet().equals(resultsToCompare.keySet())) {
             throw new IllegalArgumentException("Reference results and results to compare have different page sets");
         }
-        Map<String, UrlComparison> result = new HashMap<>();
+        LinkedHashMap<String, UrlComparison> result = new LinkedHashMap<>();
         for (String page : referenceResults.keySet()) {
             result.put(page, new UrlComparison(
                     referenceResults.get(page),
@@ -109,7 +107,7 @@ public class TestResultComparator implements Callable<Integer> {
     }
 
     private static void writeXlsx(
-            @NotNull Map<String, UrlComparison> pageComparisonMap,
+            @NotNull LinkedHashMap<String, UrlComparison> pageComparisonMap,
             @NotNull Path outputFile,
             @NotNull String sheetName
     ) throws Exception {
@@ -119,25 +117,37 @@ public class TestResultComparator implements Callable<Integer> {
                         .map(entry -> {
                             ArrayList<Object> result = new ArrayList<>();
                             result.add(entry.getKey());
-                            result.add(entry.getValue().getReference().getAverage());
-                            result.add(entry.getValue().getChanged().getAverage());
-                            result.add(entry.getValue().getReference().getMedian());
-                            result.add(entry.getValue().getChanged().getMedian());
+                            int oldAverage = entry.getValue().getReference().getAverage();
+                            result.add(oldAverage);
+                            int newAverage = entry.getValue().getChanged().getAverage();
+                            result.add(newAverage);
+                            result.add(newAverage - oldAverage);
+                            result.add((newAverage - oldAverage) * 100 / oldAverage);
+                            int oldMedian = entry.getValue().getReference().getMedian();
+                            result.add(oldMedian);
+                            int newMedian = entry.getValue().getChanged().getMedian();
+                            result.add(newMedian);
+                            result.add(newMedian - oldMedian);
+                            result.add((newMedian - oldMedian) * 100 / oldMedian);
                             return result;
                         })
                         .collect(Collectors.toList()),
                 "Page",
-                "average before",
-                "average after",
-                "median before",
-                "median after"
+                "average before, ms",
+                "average after, ms",
+                "average change, ms",
+                "average change, %",
+                "median before, ms",
+                "median after, ms",
+                "median change, ms",
+                "median change, %"
         );
         Files.write(outputFile, xlsBytes);
     }
 
     @Override
     public Integer call() throws Exception {
-        Map<String, UrlComparison> pageComparisonMap = compareResults(
+        LinkedHashMap<String, UrlComparison> pageComparisonMap = compareResults(
                 loadResults(Paths.get(referenceFile)),
                 loadResults(Paths.get(fileToCompare))
         );
